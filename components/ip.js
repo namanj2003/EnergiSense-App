05504c7ac2aecea233df2a21e08064067bb6a069
Naman Jain <naman11@somaiya.edu>
2024-04-19 22:54:11 +0530

    Update ip.js

--- components/ip.js
+++ components/ip.js
@@ -1,4 +1,3 @@
 
 const ip = 'namanjain.me';
-// https://blr1.blynk.cloud/external/api/data/get?token=BNOvMV-MK1Sgt31qwZ8NCSL7TRSQKnJI&period=DAY&granularityType=HOUR&sourceType=AVG&tzName=Asia/Kolkata&format=ISO_SIMPLE&output=JSON&pin=v0
 export default ip;
